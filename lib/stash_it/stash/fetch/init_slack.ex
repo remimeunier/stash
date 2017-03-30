7c9ea6e1c5c102637be28d5589443c3ad9588c35
Rémi Meunier <meunier.remi.p@gmail.com>
2017-03-30 21:15:29 +0300

    change recursive sql search by recursive enum search

--- lib/stash_it/stash/fetch/init_slack.ex
+++ lib/stash_it/stash/fetch/init_slack.ex
@@ -5,10 +5,10 @@ defmodule StashIt.Stash.Fetch.InitSlack do
 
  def init(team) do
   init_members(team)
-  init_channels(team)
+  init_channels(Stash.get_members(team), team)
  end
 
- defp init_members(team) do
+ def init_members(team) do
     Slack.Web.Users.list(%{token: team.token})
     |> Map.get("members")
     |> Enum.each(fn(member) ->
@@ -16,13 +16,13 @@ defmodule StashIt.Stash.Fetch.InitSlack do
     end)
  end
 
-  defp init_channels(team) do
+  defp init_channels(members, team) do
     Slack.Web.Channels.list(%{token: team.token})
     |> Map.get("channels")
     |> Enum.each(fn(channel) ->
       case Stash.create_channel(%{team_id: team.id, name: channel["name"], provider_id: channel["id"]}) do
         {:ok, channel_object} ->
-          init_links(team, channel_object)
+          init_links(members, team, channel_object)
         {:error, _} ->
           #TODO hundle error
       end
@@ -30,46 +30,46 @@ defmodule StashIt.Stash.Fetch.InitSlack do
   end
 
   #TODO refactor next fonctions
-  defp init_links(team, channel, oldest) do
-    new_fonction(Slack.Web.Channels.history(channel.provider_id, %{token: team.token, count: 1000, latest: oldest}), team, channel)
+  defp init_links(members, team, channel, oldest) do
+    new_fonction(Slack.Web.Channels.history(channel.provider_id, %{token: team.token, count: 1000, latest: oldest}), members, team, channel)
   end
 
-  defp init_links(team, channel) do
-    new_fonction(Slack.Web.Channels.history(channel.provider_id, %{token: team.token, count: 1000}), team, channel)
+  defp init_links(members, team, channel) do
+    new_fonction(Slack.Web.Channels.history(channel.provider_id, %{token: team.token, count: 1000}), members, team, channel)
   end
 
-  defp new_fonction(%{"has_more" => true } = json, team, channel) do
+  defp new_fonction(%{"has_more" => true } = json, members, team, channel) do
     json
     |> Map.get("messages")
-    |> Enum.each(&get_link(&1, team, channel))
+    |> Enum.each(&get_link(&1, members, team, channel))
 
     test = json
     |> Map.get("messages")
 
     oldest = Enum.at(test, -1)
     |> Map.get("ts")
-    init_links(team, channel, oldest)
+    init_links(members, team, channel, oldest)
   end
 
-  defp new_fonction(%{"has_more" => false } = json, team, channel) do
+  defp new_fonction(%{"has_more" => false } = json, members, team, channel) do
     json
     |> Map.get("messages")
-    |> Enum.each(&get_link(&1, team, channel))
+    |> Enum.each(&get_link(&1, members, team, channel))
   end
 
   #TODO choose between 'text' Or 'Title' in attachment
-  defp get_link(%{"user" => user_slack_id, "text" => message_text, "ts" => time_stamp, "attachments" => [%{"from_url" => url, "title" => attachment_text , "service_name" => service_name}, ]} = mess, team, channel) do
+  defp get_link(%{"user" => user_slack_id, "text" => message_text, "ts" => time_stamp, "attachments" => [%{"from_url" => url, "title" => attachment_text , "service_name" => service_name}, ]} = mess, members, team, channel) do
     #%{ name: b, url: a, service: c, poster: d}
      Stash.create_link(%{link: url,
                         link_message: attachment_text,
                         link_service_name: service_name,
                         text_message: message_text,
                         time_stamp: time_stamp,
-                        poster_id: Stash.get_member(team, user_slack_id).id,
+                        poster_id: Enum.filter_map(members, fn(x) -> x.provider_id == user_slack_id end, &(&1.id)) |> Enum.at(0), #Stash.get_member(team, user_slack_id).id,
                         channel_id: channel.id })
   end
 
   #Maybe TO DO handle one arg missing
-  defp get_link(a, b, c) do
+  defp get_link(a, b, c, d) do
   end
 end
