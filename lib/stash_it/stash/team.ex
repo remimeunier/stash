89c783da330cbeba8ded3aae5cf67ead3e62a401
Rémi Meunier <meunier.remi.p@gmail.com>
2017-05-10 16:00:29 +0300

    oath slack

--- lib/stash_it/stash/team.ex
+++ lib/stash_it/stash/team.ex
@@ -7,6 +7,8 @@ defmodule StashIt.Stash.Team do
     field :token, :string
     field :valide, :boolean
     field :name, :string
+    field :provider_team_id, :string
+    field :provider_user_id, :string
     belongs_to :user, StashIt.Accounts.User
     has_many :members, StashIt.Stash.User, on_delete: :delete_all
     has_many :channels, StashIt.Stash.Channel, on_delete: :delete_all
@@ -20,7 +22,7 @@ defmodule StashIt.Stash.Team do
 
   def team_changeset(team, attrs) do
     team
-    |> cast(attrs, [:token, :provider, :user_id, :valide, :name])
-    |> validate_required([:token, :provider, :name])
+    |> cast(attrs, [:token, :provider, :user_id, :valide, :name, :provider_team_id, :provider_user_id])
+    |> validate_required([:token, :provider, :name, :provider_team_id, :provider_user_id])
   end
 end
--- lib/stash_it/web/controllers/auth_controller.ex
+++ lib/stash_it/web/controllers/auth_controller.ex
@@ -2,6 +2,10 @@ defmodule StashIt.Web.AuthController do
   @moduledoc """
   Auth controller responsible for handling Ueberauth responses
   """
+  require Logger
+  import Guardian.Plug
+  alias StashIt.Stash
+  alias StashIt.Stash.Team
 
   use StashIt.Web, :controller
   plug Ueberauth
@@ -12,34 +16,25 @@ defmodule StashIt.Web.AuthController do
     render(conn, "request.html", callback_url: Helpers.callback_url(conn))
   end
 
-  def delete(conn, _params) do
+  def callback(%{assigns: %{ueberauth_failure: fails}} = conn, _params) do
     conn
-    |> put_flash(:info, "You have been logged out!")
-    |> configure_session(drop: true)
-    |> redirect(to: "/")
-  end
-
-  def callback(%{assigns: %{ueberauth_failure: _fails}} = conn, _params) do
-    conn
-    |> put_flash(:error, "Failed to authenticate.")
-    |> redirect(to: "/")
+     |> assign(:message, fails)
+     |> assign(:callback_url, Helpers.callback_url(conn))
+     |> render("request.html")
   end
 
   def callback(%{assigns: %{ueberauth_auth: auth}} = conn, _params) do
-    # case UserFromAuth.find_or_create(auth) do
-    #   {:ok, user} ->
-    #     conn
-    #     |> put_flash(:info, "Successfully authenticated.")
-    #     |> put_session(:current_user, user)
-    #     |> redirect(to: "/")
-    #   {:error, reason} ->
-    #     conn
-    #     |> put_flash(:error, reason)
-    #     |> redirect(to: "/")
-    # end
-    conn
-     |> assign(:message, auth)
-     |> assign(:callback_url, Helpers.callback_url(conn))
-     |> render("request.html")
+    case Stash.create_team(%{user_id: current_resource(conn).id,
+                             provider: 1,
+                             name: auth.credentials.other.team,
+                             provider_team_id: auth.credentials.other.team_id,
+                             provider_user_id: auth.credentials.other.user_id,
+                             token: auth.credentials.token}) do
+      {:ok, team} ->
+        Stash.Fetch.InitSlack.init(team)
+        redirect conn, to: "/"
+      {:error, %Ecto.Changeset{} = changeset} ->
+        # TO DO error
+    end
   end
 end
